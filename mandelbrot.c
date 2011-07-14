554d60995420ea7914fc6750533c581dfc8de21e
jonathan <jwringstad@gmail.com>
2011-07-14 02:15:04 +0200

    removed old unused function

--- mandelbrot.c
+++ mandelbrot.c
@@ -23,29 +23,6 @@ void run_renderingp(targets_t target_list) {
     destroy_surface(surface);
 }
 
-void run_rendering(surface_t* surface) {
-    real zoomfactor = 300;
-    complexreal origin = - 0.743643887037151 + 0.131825904205330*I; 
-    real iteration_depth = 100;
-    int counter = 0;
-    while(1) {
-        if(zoomfactor > 300000000000 || counter == 10) {
-            exit(0);
-        }
-        render_frame(surface, zoomfactor, origin, iteration_depth);
-
-        zoomfactor *= 1.01;
-        iteration_depth += 2.0;
-        printf("iteration depth: %e, iterations: %e\n", iteration_depth, surface->w*surface->h*iteration_depth);
-
-        char output_file[100];
-        snprintf(output_file, 100, "out_%04d.bmp", counter);
-        write_bmp(surface, output_file);
-        //printf("%s\n", output_file);
-        counter++;
-    }
-}
-
 void render_frame(surface_t* surface, real zoomfactor, complexreal origin, real iteration_depth){
     for(unsigned int x=0;  x < surface->w; x++) {
         for(unsigned int y=0;  y < surface->h; y++) {
