e8ce6b0ceb8682cf44b104698f625a696e8cab40
jonathan <jwringstad@gmail.com>
2011-07-14 04:10:00 +0200

    gmp works

--- mandelbrot.c
+++ mandelbrot.c
@@ -36,31 +36,66 @@ void render_frame(surface_t* surface, real zoomfactor, real origin_r, real origi
   }
 }
 
-real iterate_complex(real c_r, real c_i, int max_iteration) {
+real iterate_complex(real in_c_r, real in_c_i, int max_iteration) {
     int iteration_step = 0;
-    /* mpf_t z_r; */
-    /* mpf_t z_i; */
-    /* mpf_init(z_r); */
-    /* mpf_init(z_i); */
-    /* mpf_set_d(z_r, 0.0); */
-    /* mpf_set_d(z_i, 0.0); */
-    real z_r = 0.0;
-    real z_i = 0.0;
+    mpf_t z_r;
+    mpf_t z_i;
+    mpf_t c_r;
+    mpf_t c_i;
+    mpf_t tmp_a;
+    mpf_t tmp_b;
+    mpf_t tmp_c;
+    mpf_init(z_r);
+    mpf_init(z_i);
+    mpf_init(c_r);
+    mpf_init(c_i);
+    mpf_init(tmp_a);
+    mpf_init(tmp_b);
+    mpf_init(tmp_c);
+    mpf_set_d(z_r, 0.0);
+    mpf_set_d(z_i, 0.0);
+    mpf_set_d(c_r, in_c_r); // these should probably be passed in directly as mpf_t.
+    mpf_set_d(c_i, in_c_i);
+    /* real z_r = 0.0; */
+    /* real z_i = 0.0; */
     
     while(iteration_step < max_iteration){
-      real xtemp = z_r*z_r - z_i*z_i + c_r;
-      z_i = 2*z_r*z_i + c_i;
-      z_r = xtemp;
+      //real xtemp = z_r*z_r - z_i*z_i + c_r;
+      mpf_mul(tmp_a, z_r, z_r); // we should really save the results from z_r^2 and z_i^2, since we re-use them
+      mpf_mul(tmp_b, z_i, z_i);
+      mpf_sub(tmp_c, tmp_a, tmp_b);
+      mpf_add(tmp_a, tmp_c, c_r);
+      // result is now in tmp_a.
+
+      mpf_mul(tmp_b, z_r, z_i);
+      mpf_mul_ui(tmp_c, tmp_b, 2);
+      mpf_add(z_i, tmp_c, c_i);
+      //z_i = 2*z_r*z_i + c_i;
+      //z_r = xtemp;
+      mpf_set(z_r, tmp_a);
+      // tmp_a and tmp_b can now be used freely again.
+
       iteration_step++;
       
       // abort condition
-      real magsquared = z_r*z_r + z_i*z_i;
-      if(magsquared >= 4)
+      //real magsquared = z_r*z_r + z_i*z_i;
+      mpf_mul(tmp_a, z_r, z_r);
+      mpf_mul(tmp_b, z_i, z_i);
+      mpf_add(tmp_a, tmp_a, tmp_b);
+      // convert to real...?
+      real magsquared = mpf_get_d(tmp_a);
+      //      gmp_printf("magsquared: %.50ff vs %.50Ff\n", magsquared, tmp_a);
+      if(magsquared >= 4){
 	break;
+      }
     }
 
-    /* mpf_clear(z_r); */
-    /* mpf_clear(z_i); */
+    mpf_clear(z_r);
+    mpf_clear(z_i);
+    mpf_clear(c_r);
+    mpf_clear(c_i);
+    mpf_clear(tmp_a);
+    mpf_clear(tmp_b);
 
     real scale = (real)iteration_step / max_iteration;
     return scale;
